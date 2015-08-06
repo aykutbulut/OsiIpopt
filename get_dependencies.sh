f39b6d450d17badf3584989cb60073ebbc181d35
Aykut Bulut <aykutblt@gmail.com>
2015-08-06 09:11:02 -0400

    dependency script updated.

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -7,11 +7,21 @@ svn co https://projects.coin-or.org/svn/Osi/stable/0.107/Osi Osi
 # get Ipopt
 svn co https://projects.coin-or.org/svn/Ipopt/releases/3.12.3/Ipopt Ipopt
 # IPOPT depends the following
-svn co https://projects.coin-or.org/svn/BuildTools/ThirdParty/ASL/stable/1.3    ThirdParty/ASL
 svn co https://projects.coin-or.org/svn/BuildTools/ThirdParty/Blas/stable/1.4   ThirdParty/Blas
-svn co https://projects.coin-or.org/svn/BuildTools/ThirdParty/HSL/stable/1.5    ThirdParty/HSL
+cd ThirdParty/Blas
+./get.Blas
+cd ..
 svn co https://projects.coin-or.org/svn/BuildTools/ThirdParty/Lapack/stable/1.5 ThirdParty/Lapack
+cd ThirdParty/Lapack
+./get.Lapack
+cd ..
 svn co https://projects.coin-or.org/svn/BuildTools/ThirdParty/Metis/stable/1.3  ThirdParty/Metis
+cd ThirdParty/Metis
+./get.Metis
+cd ..
 svn co https://projects.coin-or.org/svn/BuildTools/ThirdParty/Mumps/stable/1.5  ThirdParty/Mumps
+cd ThirdParty/Mumps
+./get.Mumps
+cd ..
 # get osiconic
 git clone https://github.com/aykutbulut/OSI-CONIC OsiConic
